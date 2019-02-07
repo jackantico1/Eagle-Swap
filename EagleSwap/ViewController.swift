4bea73cc527a36b70ad4b979f9a26393c8fe3f7b
Jack Antico <anticoj@bc.edu>
2019-02-07 16:18:29 -0500

    Initial Commit

---
+++ EagleSwap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EagleSwap
+//
+//  Created by Jack Antico on 2/7/19.
+//  Copyright © 2019 Jack Antico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
